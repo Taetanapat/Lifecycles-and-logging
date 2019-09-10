ceadbb10e7d0d43be560c81161db06edc33540fe
Taetanapat <tanapat_5365@hotmail.com>
2019-09-10 16:33:21 +0700

    onCreate & onStart

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -77,6 +78,7 @@ class MainActivity : AppCompatActivity() {
 
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
+        Log.i("MainActivity", "onCreate Called")
     }
 
     /**
@@ -93,6 +95,11 @@ class MainActivity : AppCompatActivity() {
 
         // Show the next dessert
         showCurrentDessert()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart Called")
     }
 
     /**
